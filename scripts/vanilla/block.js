ceeeebfdc57fec2329dff6b332c4109a280d15d3
Jab <164631211+Jab005@users.noreply.github.com>
2024-06-29 11:07:08 +0700

    Add files via upload

---
+++ scripts/vanilla/block.js
@@ -0,0 +1,5 @@
+let atr = require("../content/attribute");
+
+Blocks.carbonStone.attributes.set(Attribute.carbonic, 1);
+Blocks.carbonWall.attributes.set(Attribute.carbonic, 1);
+Blocks.graphiticWall.attributes.set(Attribute.carbonic, 1.4)
