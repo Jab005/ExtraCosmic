e6dd7a8bb3283e4b8d860708b9d49a5e11c31757
Jab <164631211+Jab005@users.noreply.github.com>
2024-06-29 11:04:37 +0700

    main.js
    Goofy JavaScript-'ed mod ahh

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,7 @@
-let vteam = require("vanilla/team");
+let loadModExtraData = [
+  require("vanilla/team"),
+  require("content/attribute"),
+  require("vanilla/block"),
+  require("content/item"),
+  require("content/liquid")
+];
