6ed6c17e67cf1b6d8b78022620f9602bffde7509
Jab <164631211+Jab005@users.noreply.github.com>
2024-06-29 12:38:59 +0700

    Delete scripts/vanilla/block.js
    Error bro..

--- scripts/vanilla/block.js
+++
@@ -1,5 +0,0 @@
-let atr = require("../content/attribute");
-
-Blocks.carbonStone.attributes.set(Attribute.carbonic, 1);
-Blocks.carbonWall.attributes.set(Attribute.carbonic, 1);
-Blocks.graphiticWall.attributes.set(Attribute.carbonic, 1.4)
